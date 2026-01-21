07094b3627b62aad7f3f137687dbb368b745df33
Vallepu Vishnuvardhan <vishnu.vallepu002@gmail.com>
2026-01-21 21:56:16 +0530

    Create StudentManagementSystem.java

---
+++ Java/StudentManagementSystem.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+public class StudentManagementSystem {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    students.add(new Student(id, name));
+                    System.out.println("Student added successfully.");
+                    break;
+
+                case 2:
+                    System.out.println("Student List:");
+                    for (Student s : students) {
+                        System.out.println("ID: " + s.id + ", Name: " + s.name);
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
